582c864456e388b3e8bfe6fb97d55d22bf60d1a4
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-11-23 17:17:27 -0600

    initial commit and implementation

---
+++ README.md
@@ -0,0 +1,56 @@
+# digest-stream
+
+Simple pass-through stream (RW) which calculates the a crypto digest (sha/md5 hash) of a stream and also the length. Pipe your stream through this to get digest and length.
+
+[![Build Status](https://secure.travis-ci.org/jeffbski/digest-stream.png?branch=master)](http://travis-ci.org/jeffbski/digest-stream)
+
+## Installation
+
+
+```bash
+npm install digest-stream
+```
+
+## Usage
+
+Provide a the digest algorithm, digest encoding, and a listener function when you construct the stream. The listener will be called with the resultant digest and length of the stream just prior to end being emitted.
+
+Since this uses the node.js crypto package, refer to http://nodejs.org/api/crypto.html for the specific options available.
+
+ - `digestStream(algorithm, [inputEncoding,] digestEncoding, listenerFn)` - constructs a new stream instance, the listenerFn will be called prior to the `end` event being emitted. `inputEncoding` is optional and defaults to `binary` if not provided.
+ - `listenerFn` function signature is `fn(digest, dataLength)` - `digest` is the digest for the stream data in the digest encoding format specified when instance was created. `dataLength` is the length of the data in the stream (provided for convenience).
+
+```javascript
+var digestStream = require('digest-stream');
+var digest;
+var dataLength;
+function listenerFn(resultDigest, length) {
+  digest = resultDigest;
+  dataLength = length;
+}
+var dstream = digestStream('sha1', 'hex', listenerFn); // create instance
+readstream
+  .pipe(dstream) // digest and length calculated as it passes through
+  .pipe(...)
+```
+
+## Goals
+
+ - Easy to use pass-through stream which calculates the digest and length of string or Buffer streamlength of the string
+ - Builds on pass-stream to have all the normal pass-through functionality for a spec compliant stream
+
+## Why
+
+By making this simple pass-through stream which calculates the digest and length of a stream, it becomes really easy to add this functionality to a streaming workflow, just by piping it through this stream, the digest and length will be available prior to `end` being fired.
+## Get involved
+
+If you have input or ideas or would like to get involved, you may:
+
+ - contact me via twitter @jeffbski  - <http://twitter.com/jeffbski>
+ - open an issue on github to begin a discussion - <https://github.com/jeffbski/digest-stream/issues>
+ - fork the repo and send a pull request (ideally with tests) - <https://github.com/jeffbski/digest-stream>
+
+## License
+
+ - [MIT license](http://github.com/jeffbski/digest-stream/raw/master/LICENSE)
+
---
+++ test/basic.mocha.js
@@ -0,0 +1,150 @@
+/*global suite:false test:false */
+'use strict';
+
+var crypto = require('crypto');
+var chai = require('chai-stack');
+var digestStream = require('..'); // require('digest-stream');
+var passStream = require('pass-stream');
+
+var t = chai.assert;
+
+suite('basic');
+
+var EXPECTED_SHA1_HEX = crypto.createHash('sha1').update('abcdefghi').digest('hex');
+var EXPECTED_MD5_BASE64 = crypto.createHash('md5').update('abcdefghi').digest('base64');
+
+test('basic use string, results with digest and length being provided to listener before end', function (done) {
+  var resultDigest;
+  var resultantLength;
+  function listenerFn(digest, length) {
+    resultDigest = digest;
+    resultantLength = length;
+  }
+  var ls = digestStream('sha1', 'hex', listenerFn);
+  var stream = passStream();
+  var accumData = [];
+  stream
+    .pipe(ls)
+    .on('error', function (err) { done(err); })
+    .on('data', function (data) { accumData.push(data); })
+    .on('end', function () {
+      t.deepEqual(accumData, ['abc', 'def', 'ghi']);
+      t.equal(resultDigest, EXPECTED_SHA1_HEX);
+      t.equal(resultantLength, 9);
+      done();
+    });
+  process.nextTick(function () {
+    stream.write('abc');
+    stream.write('def');
+    stream.end('ghi');
+  });
+});
+
+test('basic use Buffer, results with digest and length being provided to listener before end', function (done) {
+  var resultDigest;
+  var resultantLength;
+  function listenerFn(digest, length) {
+    resultDigest = digest;
+    resultantLength = length;
+  }
+  var ls = digestStream('sha1', 'hex', listenerFn);
+  var stream = passStream();
+  var accumData = [];
+  stream
+    .pipe(ls)
+    .on('error', function (err) { done(err); })
+    .on('data', function (data) { accumData.push(data); })
+    .on('end', function () {
+      t.deepEqual(accumData, [new Buffer('abc'), new Buffer('def'), new Buffer('ghi')]);
+      t.equal(resultDigest, EXPECTED_SHA1_HEX);
+      t.equal(resultantLength, 9);
+      done();
+    });
+  process.nextTick(function () {
+    stream.write(new Buffer('abc'));
+    stream.write(new Buffer('def'));
+    stream.end(new Buffer('ghi'));
+  });
+});
+
+test('basic use Buffer, md5/base64 results with digest and length being provided to listener before end', function (done) {
+  var resultDigest;
+  var resultantLength;
+  function listenerFn(digest, length) {
+    resultDigest = digest;
+    resultantLength = length;
+  }
+  var ls = digestStream('md5', 'base64', listenerFn);
+  var stream = passStream();
+  var accumData = [];
+  stream
+    .pipe(ls)
+    .on('error', function (err) { done(err); })
+    .on('data', function (data) { accumData.push(data); })
+    .on('end', function () {
+      t.deepEqual(accumData, [new Buffer('abc'), new Buffer('def'), new Buffer('ghi')]);
+      t.equal(resultDigest, EXPECTED_MD5_BASE64);
+      t.equal(resultantLength, 9);
+      done();
+    });
+  process.nextTick(function () {
+    stream.write(new Buffer('abc'));
+    stream.write(new Buffer('def'));
+    stream.end(new Buffer('ghi'));
+  });
+});
+
+test('basic use string with inputEncoding utf8, results with listenerFn(digest, len) before end', function (done) {
+  var resultDigest;
+  var resultantLength;
+  function listenerFn(digest, length) {
+    resultDigest = digest;
+    resultantLength = length;
+  }
+  var ls = digestStream('sha1', 'utf8', 'hex', listenerFn);
+  var stream = passStream();
+  var accumData = [];
+  stream
+    .pipe(ls)
+    .on('error', function (err) { done(err); })
+    .on('data', function (data) { accumData.push(data); })
+    .on('end', function () {
+      t.deepEqual(accumData, ['abc', 'def', 'ghi']);
+      t.equal(resultDigest, EXPECTED_SHA1_HEX);
+      t.equal(resultantLength, 9);
+      done();
+    });
+  process.nextTick(function () {
+    stream.write('abc');
+    stream.write('def');
+    stream.end('ghi');
+  });
+});
+
+test('all arguments missing for factory, throws error', function () {
+  function throwsErr() {
+    var stream = digestStream();
+  }
+  t.throws(throwsErr, /digestStream requires algorithm, digestEncoding, and listenerFn/);
+});
+
+test('1 arg provided, throws error', function () {
+  function throwsErr() {
+    var stream = digestStream('sha1');
+  }
+  t.throws(throwsErr, /digestStream requires algorithm, digestEncoding, and listenerFn/);
+});
+
+test('2 arg provided, throws error', function () {
+  function throwsErr() {
+    var stream = digestStream('sha1', 'hex');
+  }
+  t.throws(throwsErr, /digestStream requires algorithm, digestEncoding, and listenerFn/);
+});
+
+test('listenerFn not a function, throws error', function () {
+  function throwsErr() {
+    var stream = digestStream('sha1', 'binary', 'hex', {});
+  }
+  t.throws(throwsErr, /listenerFn needs to be a function/);
+});
